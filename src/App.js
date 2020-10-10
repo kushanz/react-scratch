b6c01478ab7a8e37e0a564727b2ec5775a693729
kushanz <kushanshanuka@yahoo.com>
2020-10-10 11:07:00 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./style.css";
 
+// pages
+import Header from './pages/Header'
+
 export default function App() {
   return (
     <div>
+    <Header title="Header Copmonent"/>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
