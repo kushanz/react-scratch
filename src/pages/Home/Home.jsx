14eb1cafeb62ebc3c594d7f435eb9dea493c5e0c
kushanz <kushanshanuka@yahoo.com>
2021-05-07 22:45:11 +0530

    1 file modified

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Home() {
-
+  const [count,setCount] = useState(0)
+  const [name,setName] = useState({fname:"",lname:""})
+  const incr = () => {
+    setCount(count+1)
+  }
   return (
     <div>
       <h2>This is Home page</h2>
+      <h2>{ count }</h2>
+      <button onClick={incr}>Content</button>
+      <p>
+        <input type='text' value={name.fname} onChange={e => setName({...name,fname:e.target.value})} /> 
+        <input type='text' value={name.lname} onChange={e => setName({...name,lname:e.target.value})} /> 
+      </p>
+      <p>{JSON.stringify(name)}</p>
     </div>
   )
 }
