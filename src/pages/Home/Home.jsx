3027ccbfcc82c005ccddf5b078bf93961a5467a8
kushanz <kushanshanuka@yahoo.com>
2021-05-08 21:34:32 +0530

    2 files modified

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,16 @@ import React, {useState} from "react";
 export default function Home() {
   const [count,setCount] = useState(0)
   const [name,setName] = useState({fname:"",lname:""})
+  const [items, setItems] = useState([])
   const incr = () => {
     setCount(count+1)
+  }
+
+  const addItems = () => {
+    setItems([...items,{
+      id: items.length,
+      title: "item"+items.length
+    }])
   }
   return (
     <div>
@@ -16,6 +24,14 @@ export default function Home() {
         <input type='text' value={name.lname} onChange={e => setName({...name,lname:e.target.value})} /> 
       </p>
       <p>{JSON.stringify(name)}</p>
+      <hr />
+      <button onClick={addItems}>Add Items</button>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>{item.title}</li>
+  ))}
+      </ul>
+      {/* <pre>{JSON.stringify(items)}</pre> */}
     </div>
   )
 }
--- src/pages/Peoples/Peoples.jsx
+++ src/pages/Peoples/Peoples.jsx
@@ -1,11 +1,18 @@
-import React from "react"
+import React, {useState,useEffect} from "react"
 
 export default function Peoples() {
+  const [count, setCount] = useState(0)
 
+  const counterInc = () => {
+    setCount(count+ 1)
+  }
+  useEffect(() => {
+    document.title = `click Me ${count}`
+  })
   return (
     <div>
       <h1>Loading Peoples</h1>
-      
+      <button onClick={counterInc}>Click Me </button>
     </div>
   ) 
 }
